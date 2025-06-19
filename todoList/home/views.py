8a178db7e4820030d97ba3588644a85edfd10ba4
fenilm57 <fenilm57@gmail.com>
2025-06-19 02:45:49 -0400

    Insert and view task are done

--- todoList/home/views.py
+++ todoList/home/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from home.models import Task
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'success': False}
+    if request.method == 'POST':
+        title = request.POST['title']
+        desc = request.POST['desc']
+        ins = Task(
+            title=title,
+            description=desc
+        )
+        ins.save()
+        return redirect('/?success=1')
+
+    success = request.GET.get('success') == '1'
+    return render(request, 'home.html', {'success': success})
 
 
 def showList(request):
-    return render(request, 'showList.html')
+    tasks = Task.objects.all()
+    return render(request, 'showList.html', {'tasks': tasks})
+
+
+def delete_task(request):
+    if request.method == 'POST':
+        id = request.POST['id']
+        task = Task(id=id)
+        task.delete()
+        print(redirect)
+    return render(request, 'home.html')
